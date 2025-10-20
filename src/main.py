9c485c1e7324153baccb5f50aea35d3f654cc28e
TheBrosky1899 <136100204+TheBrosky1899@users.noreply.github.com>
2025-10-19 20:34:37 -0500

    sadf

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import streamlit as st
 from auth.setup import setup_aws
 from auth.login import login_user_with_google
 
+from aws.s3 import S3Helper
+
 st.set_page_config(page_title="Platform Shift Hub", page_icon="🚀", layout="wide")
 
 st.title("Welcome to Platform Shift Hub")
@@ -11,6 +13,9 @@ def main():
     if user := login_user_with_google():
         st.write(user)
         setup_aws()
+
+        s3_helper: S3Helper = st.session_state.s3_helper
+        st.write(s3_helper.list_keys())
 
 
 if __name__ == "__main__":
