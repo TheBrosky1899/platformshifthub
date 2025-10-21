e12982356e4a28e60c31ea893bacfcab74b7a68c
TheBrosky1899 <136100204+TheBrosky1899@users.noreply.github.com>
2025-10-20 20:10:57 -0500

    fix login

--- src/main.py
+++ src/main.py
@@ -8,10 +8,10 @@ logger = logging.getLogger(__name__)
 
 st.set_page_config(page_title="Platform Shift Hub", page_icon="🚀", layout="wide")
 
-st.title("Welcome to Platform Shift Hub")
-
-
 def main():
+    if not st.user.is_logged_in:
+        st.title("Welcome to Platform Shift Hub")
+        st.subheader("Please log in to continue.")
     if user := login_user_with_google():
         allowed_users = json.load(open("src/admin/allowed_users.json"))
         if user.email not in allowed_users:
